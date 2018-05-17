f08ac2d6abe83ed340c486647d3da9206613feda
pac9 <pac9@btinternet.com>
2018-05-17 15:33:00 +0100

    director set up

--- src/main/java/staff/management/Director.java
+++ src/main/java/staff/management/Director.java
@@ -1,4 +1,21 @@
 package staff.management;
 
-public class Director {
+
+import staff.Employee;
+import staff.management.Manager;
+
+public class Director extends Employee {
+
+    private double budget;
+
+
+    public Director(String name, String nIno, Double salary, String deptName, Double budget){
+        super(name, nIno, salary);
+        this.deptname = deptName;
+        this.budget = budget;
+
+    }
+
+    
 }
+
